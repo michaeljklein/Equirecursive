9734e317b3af4f9969d4e218333977dd315c2b9a
michaeljklein <michael14486@gmail.com>
2016-10-01 14:18:33 -0500

    Fleshing out how to constrain the generator class

--- src/Data/Rec.hs
+++ src/Data/Rec.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE RankNTypes #-}
 
 
-module Data.Rec where
+module Data.Rec (Rec (..)) where
 
 import Control.Arrow
 import Data.Kind
@@ -15,11 +15,23 @@ data Rec (a :: *) = Rec { getRec :: a }
 t1 :: forall t. t -> (Integer, t)
 t1 = \x -> (0, x)
 
-r1 :: Rec (Int, XX k)
+r1 :: Rec (Integer, XX k)
 r1 = undefined
+
+-- | Trivial conversion
+r1' :: Rec (Integer, XX k)
+r1' = undefined
+
 
 t2 :: forall t. (Integer, t) -> (Integer, (Integer, t))
 t2 = \x -> (0, first (+1) x)
+
+r2 :: Rec (Integer, (Integer, XX k))
+r2 = undefined
+
+r2' :: Rec (Integer, XX k)
+r2' = undefined
+
 
 -- Not sure about this one
 t3 :: forall a d. (a -> (Integer, d)) -> Integer -> (Integer, a -> (Integer, d))
@@ -30,6 +42,45 @@ instance Functor ((,,) a b) where
 
 t4 :: forall t. (t, t, Integer) -> ((t, t, Integer), (t, t, Integer), Integer)
 t4 = \x -> ((*2) <$> x, (+1).(*2) <$> x, 0)
+
+r4 :: Rec ((XX k, XX k, Integer), (XX k, XX k, Integer), Integer)
+r4 = undefined
+
+r4' :: Rec (XX k, XX k, Integer)
+r4' = undefined
+
+-- there are two types of recursion:
+-- t -> c t, c t -> c (c t)
+-- The first type is effectively trivial, and is more
+-- general type-wise than the second, but the second is
+-- more general functionality wise.
+--
+-- One method is to apply to the least common denominator and always
+-- require the second. The other method is to attempt to double-up on the instances
+-- since there's a type-level iso between them. What about a value-level iso?
+
+
+-- There's really only one way to do this: We need to replace the first argument of
+-- the t-types with XX k in the entire t-type.
+-- Why only one way? Without depending on the first argument, we can't tell the difference between
+-- (1,(2,(1,(2...)))) and (1, (([_,2,1]!!), ...)).
+-- The first is static, simple, recursion. the second references previous values.
+
+
+-- Compare r4 and r4'. The type of r4 tells us that the recursive function
+-- references its input (when viewed vs. t4). The type of r4' hides that
+-- information, but may be derived with more ease?
+
+-- One clear point is that the non-prime r-types contain more information than is needed once
+-- wrapped in Rec.
+--
+-- How can we convert the t-types into the r'-types??
+
+-- Should this one be an iso?
+-- Hmmmm.. Probably not.
+-- We do need to 1) know how to get from ti -> ri, but that "might" be as easy as (`asTypeOf` t4 xX)
+-- 2) do we need to be able to get back?
+
 -- class Recurse
 
 
