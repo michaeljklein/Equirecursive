b2543eaf2d3bfec907821039edff09166c55ddc9
michaeljklein <michael14486@gmail.com>
2016-10-23 23:13:41 -0500

    Figuring out how to manage the kind classes of names

--- src/Test/QuickCheck/Types/TH.hs
+++ src/Test/QuickCheck/Types/TH.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonoLocalBinds #-}
 
 module Test.QuickCheck.Types.TH where
 
@@ -10,8 +14,10 @@ import Control.Applicative         (Const)
 import Control.Applicative         (WrappedArrow)
 import Control.Applicative         (WrappedMonad)
 import Control.Applicative         (ZipList)
+import Control.Applicative         (Alternative(..))
 import Control.Arrow               (ArrowMonad)
 import Control.Arrow               (Kleisli)
+import Control.Arrow               (first)
 import Control.Exception           (AllocationLimitExceeded)
 import Control.Exception           (AssertionFailed)
 import Control.Exception           (BlockedIndefinitelyOnMVar)
@@ -157,31 +163,161 @@ import Data.Kind
 import Data.Default
 import Text.PrettyPrint
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 
--- | A `Type` with kind @k@
-data TypeK k = TypeK { getTypeK :: TH.Type, typeKind :: Kind } deriving (Eq, Ord, Show)
+-- TODO: Need to make: Arbitrary, CoArbitrary, using GenS to prevent blowup
+-- Cleanup imports
+-- Migrate most of this to its own module, here we should really just have
+--  the type family -> TH -> test functions.
+-- Remember to make quasiquoters for Limbo once this is all done.
 
+-- TODO: GenS elements/oneOf need to decrement state when traversing!
+
+-- | A TemplateHaskell `Type` with kind @k@
+data TypeK k = TypeK { getTypeK :: TH.Type, typeKind :: Maybe Kind } deriving (Eq, Ord, Show)
+
+-- | There has to be a better way to defined `ppr` for this
 instance Ppr (TypeK k) where
-  ppr (TypeK ty kind) = liftM2 (<>) (liftM2 (<>) (liftM2 (<>) (return "TypeK ") $ ppr ty) $ return " ") $ ppr kind
+  ppr (TypeK ty (Just kind)) = liftM2 (<>) (liftM2 (<>) (liftM2 (<>) (return "TypeK ") $ ppr ty) $ return " ") $ ppr kind
+  ppr (TypeK ty (_        )) = liftM2 (<>) (liftM2 (<>) (return "TypeK ") $ ppr ty) $ return " "
 
+-- | Convenience synonym
 type TypeKQ k = Q (TypeK k)
 
+-- -- | Apply one `TypeK` to another
+-- kApp :: TypeK (k0 -> k1) -> TypeK k0 -> TypeK k1
+-- kApp (TypeK tyf kf) (TypeK tyx kx) = TypeK (AppT tyf tyx) ky
+--   where
+--     ky = (join $ liftM2 simpleApp kf kx) <|> (join $ liftM2 polyApp kf kx)
 
--- | Apply one `TypeK` to another
-kApp :: TypeK (k0 -> k1) -> TypeK k0 -> TypeK k1
-kApp = undefined
+-- kapp :: TypeKQ (k0 -> k1) -> TypeKQ k0 -> TypeKQ k1
+-- kapp = liftM2 kApp
 
+
+-- TODO: Fail better
 -- | Make a `TypeKQ`
 typeK :: ExpQ -> X k -> Name -> TypeKQ k
 typeK e _ n = do
   e' <- e
   case e' of
-    SigE (VarE _) (AppT _ kind) -> return $ TypeK (ConT n) kind
+    SigE (VarE _) (AppT _ kind) -> return $ TypeK (ConT n) (Just kind)
+    SigE (VarE _) (ForallT tys cxt (AppT _ kind)) -> return $ TypeK (ConT n) (Just $ ForallT tys cxt kind)
     other              -> fail . show $ other
+
 
 -- | Make a bunch of `TypeKQ`'s
 typeKs :: (ExpQ, X k, [Name]) -> [TypeKQ k]
 typeKs (e, x, ns) = typeK e x <$> ns
+
+-- | Valid applications return `Just`, others return `Nothing`
+type family MaybeAppK (a :: Type) (b :: Type) :: Maybe Type where
+  MaybeAppK (k0 -> k) k0 = 'Just k
+  MaybeAppK  k0       k1 = 'Nothing
+
+-- | This class allows applying arbitrary `TypeK`'s to each other.
+-- When it's an invalid application, it returns @forall t. Maybe (TypeK t)@
+class (MaybeAppK a b ~ m) => TypeKApp (a :: Type) (b :: Type) (m :: Maybe Type) where
+  type DropMaybe t a b m :: Type
+  maybeAppK' :: forall t. TypeK a -> TypeK b -> Maybe (TypeK (DropMaybe t a b m))
+
+  maybeAppK  :: forall t. TypeKQ a -> TypeKQ b -> MaybeT Q (TypeK (DropMaybe t a b m))
+
+
+instance (MaybeAppK a b ~ 'Just k) => TypeKApp a b ('Just k) where
+  type DropMaybe t a b ('Just k) = k
+  maybeAppK' :: TypeK a -> TypeK b -> Maybe (TypeK k)
+  maybeAppK' (TypeK tyf kf) (TypeK tyx kx) = Just (TypeK (AppT tyf tyx) ky)
+    where
+      ky = (join $ liftM2 simpleApp kf kx) <|> (join $ liftM2 polyApp kf kx)
+
+  maybeAppK x y = MaybeT (liftM2 maybeAppK' x y)
+
+
+instance (MaybeAppK a b ~ 'Nothing) => TypeKApp a b 'Nothing where
+  type DropMaybe t a b 'Nothing = t
+  maybeAppK' :: forall t. TypeK a -> TypeK b -> Maybe (TypeK t)
+  maybeAppK' _ _ = Nothing
+
+  maybeAppK x y = MaybeT (liftM2 maybeAppK' x y)
+
+
+splitK :: Kind -> Maybe (Kind, Kind)
+splitK (AppT (AppT ArrowT k0) k1) = Just (k0, k1)
+splitK  _                         = Nothing
+
+simpleApp :: Kind -> Kind -> Maybe Kind
+simpleApp x y = case first (== y) <$> splitK x of
+                  Just (True, z) -> Just z
+                  _              -> Nothing
+
+polyApp :: Kind -> Kind -> Maybe Kind
+polyApp _ _ = def
+
+
+
+-- maybeAppK :: forall t. TypeK a -> TypeK b -> MaybeTypeK t a b
+-- maybeAppK = undefined
+
+
+-- anyk :: [TypeKQ k]
+--   all
+
+-- meta :: [TypeKQ (Type -> Meta -> (Type -> Type) -> Type -> Type)]
+--   `atom`
+
+-- type1 :: [TypeKQ Type]
+
+-- type2 :: [TypeKQ (Type -> Type)]
+
+-- type3 :: [TypeKQ (Type -> Type -> Type)]
+
+-- type4 :: [TypeKQ (Type -> Type -> Type -> Type)]
+--   `atom`
+
+-- type2type2 :: [TypeKQ ((Type -> Type) -> Type -> Type)]
+--   type2type2type2
+
+-- type2type2type2 :: [TypeKQ ((Type -> Type) -> (Type -> Type) -> Type -> Type)]
+--   wrappolywrap
+
+-- type2type3 :: [TypeKQ ((Type -> Type) -> Type -> Type -> Type)]
+--   `atom`
+
+-- type3type2 :: [TypeKQ ((Type -> Type -> Type) -> Type -> Type)]
+--   type3type3
+
+-- type3type3 :: [TypeKQ ((Type -> Type -> Type) -> Type -> Type -> Type)]
+--   `atom`
+
+-- typewrap :: [TypeKQ (Type -> k -> Type)]
+--   `atom`
+
+-- wrap :: [TypeKQ (k -> Type)] -- add X
+--   typewrap
+
+-- wrappolywrap :: [TypeKQ ((k -> Type) -> (k1 -> k) -> k1 -> Type)]
+--   `atom`
+
+-- wrapwrap :: [TypeKQ ((k -> Type) -> k -> Type)]
+--   wrappolywrap
+
+-- nat :: [TypeKQ Nat]
+-- nat = undefined $ (LitT . NumTyLit) <$> [0..]
+
+-- numTyLit :: Integer -> TyLitQ
+
+-- strTyLit :: String -> TyLitQ
+
+-- symbol :: [TypeKQ Symbol]
+-- symbol = undefined $ (LitT . StrTyLit) <$> ["strings"] -- ascii
+
+-- bool :: [TypeKQ Bool]
+-- bool = undefined [ PromotedT 'True, PromotedT 'False ]
+
+
+-- also, don't forget such things as: Symbol, Nat, Bool, ErrorMessage, etc.
+
 
 
 -- | List of included types should go here.
@@ -396,6 +532,9 @@ type1 = typeKs ([| def :: X( Type ) |],
   , ''Version -- :: *
   , ''Word -- :: *
   ])
+
+
+
 
 
 
